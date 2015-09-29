c2c1a2959a479bc9c61c7195b8ae26501f4addfd
Hackbright Student <info@hackbrightacademy.com>
2015-09-28 17:55:45 -0700

    sorted restaurant data

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -4,7 +4,13 @@ restaurant_data ={}
 for line in restaurant_scores:
     stripped_line = line.strip()
     split_line = stripped_line.split(":")
-#    restaurant_name = split_line[0]
-    restaurant_data(split_line[0]) = split_line[1]
+    restaurant_name = split_line[0]
+    restaurant_rating = split_line[1]
+    restaurant_data[restaurant_name] = restaurant_rating
 
-print resta
+sorted_restaurant_data = sorted(restaurant_data)
+print sorted_restaurant_data
+print restaurant_data
+for name in sorted_restaurant_data:
+    print " %s is rated at %s." % (name, restaurant_data[name])
+
